b72ac86b5586647814b82479ff2663d9b62b693b
toxcik3110 <toxcik3110@bigmir.net>
2017-08-03 23:37:26 +0300

    Add transition between RoomBrowser and Room, concept layout of Room component

--- app/components/Room.jsx
+++ app/components/Room.jsx
@@ -4,11 +4,41 @@ export class Room extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			ready:false,
+		}
 	}
 	render() {
+		var {name, creator, onClick} = this.props;
+		var {ready} = this.state;
 		return (
-			<div>
-				<h1 className='page-title'>Room</h1>
+			<div className="cardFlex fullWidth fullHeight columnOrder">
+				<div className='cardGap cardFlex centerFlex'>
+					<div className='cardGap'></div>
+					<div className='cardGap cardFlex'>
+						<button className='button large alert expanded' onClick={onClick}>
+							Canel
+						</button>
+					</div>
+					<div className='cardGap'></div>
+					<h1 className='page-title'>Select Deck</h1>
+					<div className='cardGap'></div>
+					<div className='cardGap cardFlex'>
+						<button className='button large success expanded'>
+							{!ready ? 'Ready' : 'Not ready'}
+						</button>
+					</div>
+					<div className='cardGap'></div>
+				</div>
+				<div className='cardGap5 callout secondary'>
+					Decks
+				</div>
+				<div className='cardGap'>Enemy</div>
+				<div className='cardGap'>
+					<button className='button large success expanded' disabled={true}>
+						Start game
+					</button>
+				</div>
 			</div>
 		);
 	}
--- app/components/RoomBrowser.jsx
+++ app/components/RoomBrowser.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {socket} from 'MainApp';
 import {NavLink} from 'react-router-dom';
+import Room from 'Room';
 
 export class RoomBrowser extends React.Component {
 
@@ -8,10 +9,14 @@ export class RoomBrowser extends React.Component {
 		super(props);
 		this.state = {
 			rooms: [],
+			room: undefined,
+			// room: {
+			// 	name: 'My game',
+			// 	creator: 'Toxcik',
+			// }
 		}
 		socket.emit('getRooms');
 		socket.on('rooms', (data) => {
-			// console.log('receive rooms', data);
 			this.updateRooms(data);
 		});
 		this.updateRooms = this.updateRooms.bind(this);
@@ -20,7 +25,6 @@ export class RoomBrowser extends React.Component {
 
 	updateRooms(data) {
 		if(data) {
-			// console.log('data',data);
 			this.setState({
 				rooms:[...data.rooms],
 			})
@@ -30,65 +34,135 @@ export class RoomBrowser extends React.Component {
 	}
 
 	render() {
-		var {rooms} = this.state;
+		var {rooms, room} = this.state;
 		var renderRooms = () => {
 			return rooms.map((room) => {
 				return (<div className='cardFlex justifyAround alignCenter'>
 							<h1 className='cardGap3'>{room.name}</h1>
 							<h1 className='cardGap3'><small>{room.creator}</small></h1>
-							<button className='cardGap button primary alert expanded'>Join</button>
+							<button 
+							className='cardGap button primary expanded'
+							onClick={(e) => {
+								this.setState({
+									room,
+								});
+							}}
+							>
+								Join
+							</button>
 						</div>);
 			})
 		}
-		return (
-			<div className="cardFlex fullWidth fullHeight columnOrder">
-				<div className='cardGap cardFlex'>
-					<div className='cardGap'></div>
+		if(room !== undefined) {
+			return (<Room 
+						name={room.name} 
+						creator={room.creator} 
+						onClick={(e) => {
+							this.setState({
+								room:undefined,
+							});
+						}} 
+					/>);
+		} else {
+			return (
+				<div className="cardFlex fullWidth fullHeight columnOrder">
+					<div className='cardGap cardFlex'>
+						<div className='cardGap'></div>
+						<div className='cardGap cardFlex centerFlex'>
+							<NavLink to="/">
+								<button className='button large alert expanded'>
+									Back
+								</button>
+							</NavLink>
+						</div>
+						<div className='cardGap'></div>
+						<h1 className='page-title'>Room Browser</h1>
+						<div className='cardGap'></div>
+						<div className='cardGap'></div>
+						<div className='cardGap'></div>
+					</div>
+					<div className='cardGap5 callout secondary'>
+						{renderRooms()}
+					</div>
 					<div className='cardGap cardFlex centerFlex'>
-						<NavLink to="/">
-							<button className='button large alert expanded'>
-								Back
+						<div className='cardGap'></div>
+						<div className='cardGap'>
+							<button className='button large primary expanded'>
+								{'<='}
 							</button>
-						</NavLink>
+						</div>
+						<div className='cardGap'></div>
+						<div className='cardGap5'>
+							<button className='button large success hollow expanded'>
+								{'Create game'}
+							</button>
+						</div>
+						<div className='cardGap'></div>
+						<div className='cardGap3'>
+							<button className='button large primary expanded'>
+								{'Refresh'}
+							</button>
+						</div>
+						<div className='cardGap'></div>
+						<div className='cardGap'>
+							<button className='button large primary expanded'>
+								{'=>'}
+							</button>
+						</div>
+						<div className='cardGap'></div>
 					</div>
-					<div className='cardGap'></div>
-					<h1 className='page-title'>Room Browser</h1>
-					<div className='cardGap'></div>
-					<div className='cardGap'></div>
-					<div className='cardGap'></div>
 				</div>
-				<div className='cardGap5 callout secondary'>
-					{renderRooms()}
-				</div>
-				<div className='cardGap cardFlex centerFlex'>
-					<div className='cardGap'></div>
-					<div className='cardGap'>
-						<button className='button large primary expanded'>
-							{'<='}
-						</button>
-					</div>
-					<div className='cardGap'></div>
-					<div className='cardGap5'>
-						<button className='button large success hollow expanded'>
-							{'Create game'}
-						</button>
-					</div>
-					<div className='cardGap'></div>
-					<div className='cardGap3'>
-						<button className='button large primary expanded'>
-							{'Refresh'}
-						</button>
-					</div>
-					<div className='cardGap'></div>
-					<div className='cardGap'>
-						<button className='button large primary expanded'>
-							{'=>'}
-						</button>
-					</div>
-					<div className='cardGap'></div>
-				</div>
-			</div>
-		);
+			);
+		}
+		// return (
+		// 	<div className="cardFlex fullWidth fullHeight columnOrder">
+		// 		<div className='cardGap cardFlex'>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap cardFlex centerFlex'>
+		// 				<NavLink to="/">
+		// 					<button className='button large alert expanded'>
+		// 						Back
+		// 					</button>
+		// 				</NavLink>
+		// 			</div>
+		// 			<div className='cardGap'></div>
+		// 			<h1 className='page-title'>Room Browser</h1>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap'></div>
+		// 		</div>
+		// 		<div className='cardGap5 callout secondary'>
+		// 			{renderRooms()}
+		// 		</div>
+		// 		<div className='cardGap cardFlex centerFlex'>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap'>
+		// 				<button className='button large primary expanded'>
+		// 					{'<='}
+		// 				</button>
+		// 			</div>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap5'>
+		// 				<button className='button large success hollow expanded'>
+		// 					{'Create game'}
+		// 				</button>
+		// 			</div>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap3'>
+		// 				<button className='button large primary expanded'>
+		// 					{'Refresh'}
+		// 				</button>
+		// 			</div>
+		// 			<div className='cardGap'></div>
+		// 			<div className='cardGap'>
+		// 				<button className='button large primary expanded'>
+		// 					{'=>'}
+		// 				</button>
+		// 			</div>
+		// 			<div className='cardGap'></div>
+		// 		</div>
+		// 	</div>
+		// );
 	}
 }
 
